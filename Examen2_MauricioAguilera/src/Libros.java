98a886742650dce2e941588532a4bf3ae4e3917d
JacFox11 <49622962+JacFox11@users.noreply.github.com>
2019-05-17 13:47:49 -0600

    variables

--- Examen2_MauricioAguilera/src/Libros.java
+++ Examen2_MauricioAguilera/src/Libros.java
@@ -1,3 +1,6 @@
+
+import java.util.Date;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,127 @@
  * @author mauri
  */
 public class Libros {
+    private String autor;
+    private String titulo;
+    private String genero; 
+    private String editorial;
+    private String idioma;
+    private String ISBN;
+    private String estado;
+    private String nombre;
+    private Date anioescrito;
+    private Date anioingresado;
+    private int cuenta;
+
+    public Libros() {
+    }
+
+    public Libros(String autor, String titulo, String genero, String editorial, String idioma, String ISBN, String estado, String nombre, Date anioescrito, Date anioingresado, int cuenta) {
+        this.autor = autor;
+        this.titulo = titulo;
+        this.genero = genero;
+        this.editorial = editorial;
+        this.idioma = idioma;
+        this.ISBN = ISBN;
+        this.estado = estado;
+        this.nombre = nombre;
+        this.anioescrito = anioescrito;
+        this.anioingresado = anioingresado;
+        this.cuenta = cuenta;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getAnioescrito() {
+        return anioescrito;
+    }
+
+    public void setAnioescrito(Date anioescrito) {
+        this.anioescrito = anioescrito;
+    }
+
+    public Date getAnioingresado() {
+        return anioingresado;
+    }
+
+    public void setAnioingresado(Date anioingresado) {
+        this.anioingresado = anioingresado;
+    }
+
+    public int getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(int cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    @Override
+    public String toString() {
+        return "Libros{" + "autor=" + autor + ", titulo=" + titulo + ", genero=" + genero + ", editorial=" + editorial + ", idioma=" + idioma + ", ISBN=" + ISBN + ", estado=" + estado + ", nombre=" + nombre + ", anioescrito=" + anioescrito + ", anioingresado=" + anioingresado + ", cuenta=" + cuenta + '}';
+    }
+    
     
 }
--- Examen2_MauricioAguilera/src/Usuarios.java
+++ Examen2_MauricioAguilera/src/Usuarios.java
@@ -1,13 +1,78 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author mauri
- */
+import java.util.ArrayList;
+
+
 public class Usuarios {
+    private int codigo;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    ArrayList <Libros> predtados = new ArrayList();
+    ArrayList <Libros> historial = new ArrayList();
+
+    public Usuarios() {
+    }
+
+    public Usuarios(int codigo, String nombre, String apellido, int edad) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public ArrayList<Libros> getPredtados() {
+        return predtados;
+    }
+
+    public void setPredtados(ArrayList<Libros> predtados) {
+        this.predtados = predtados;
+    }
+
+    public ArrayList<Libros> getHistorial() {
+        return historial;
+    }
+
+    public void setHistorial(ArrayList<Libros> historial) {
+        this.historial = historial;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuarios{" + "codigo=" + codigo + ", nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", predtados=" + predtados + ", historial=" + historial + '}';
+    }
+
+    
     
 }
